68a4a0b958c7962b14cff398755d5cedef1752a1
Tamsin Bowles <tamsin.b@38degrees.org.uk>
2023-01-17 15:25:15 +0000

    action action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # Application controller
 class ApplicationController < ActionController::Base
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   protect_from_forgery with: :exception
 
   before_action :check_permitted_ips
